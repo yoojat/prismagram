2bd846ed6dba7b2facbd6ceeefb210d4746b3355
yoojat <yooja.ty@gmail.com>
2019-05-08 02:54:21 +0900

    #3.3 sendMail Function with Nodemailer

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -10,7 +10,6 @@ export default {
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
       } catch (error) {
-        console.log(error);
         return false;
       }
     }
