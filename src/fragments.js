84a4856afdb2db4178a10e5516257c4d0257d11a
yoojat <yooja.ty@gmail.com>
2019-06-02 15:24:56 +0900

    seeRoom Resolver

--- src/fragments.js
+++ src/fragments.js
@@ -1,6 +1,7 @@
 export const USER_FRAGMENT = `
     id
     username
+    avatar
 `;
 
 export const COMMENT_FRAGMENT = `
@@ -14,6 +15,17 @@ export const COMMENT_FRAGMENT = `
 export const FILE_FRAGMENT = `
         id
         url
+`;
+
+export const MESSAGE_FRAGMENT = `
+    id
+    text
+    to {
+        ${USER_FRAGMENT}
+    }
+    from {
+        ${USER_FRAGMENT}
+    }
 `;
 
 export const FULL_POST_FRAGMENT = `
@@ -38,6 +50,10 @@ export const ROOM_FRAGMENT = `
         id
         participants {
             id
+            ${USER_FRAGMENT}
+        }
+        messages{
+            ${MESSAGE_FRAGMENT}
         }
     }
 `;
