278f37cd4be8c8538e5836db6c54abf52084b424
yoojat <yooja.ty@gmail.com>
2019-07-25 18:36:00 +0900

    parent를 활용한 리턴값 추가 만들기

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -14,4 +14,12 @@ export default {
       };
     }
   }
+  // ,
+  // User: {
+  //   fullName: parent => {
+  //     return `${parent.firstName} ${parent.lastName}`;
+  //   }
+  // }
+  // parent를 활용하여 데이터베이스 컬럼없이 fullName을 리턴할 수 있다
+  // src/models.graphql에는 fullName을 추가해주어야 함
 };
