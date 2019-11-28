4e98eb06d8e67341cca24ac71cd7389fc9622bb4
김태영 <kimtaeyeong@gimtaeyeong-ui-MacBookPro.local>
2019-11-28 18:19:31 +0900

    #18.8 Uploading to Backend

--- src/upload.js
+++ src/upload.js
@@ -5,7 +5,8 @@ export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   // file은 middleware에서 옴
-  const { file } = req;
-  console.log(file);
-  res.end();
+  const {
+    file: { path }
+  } = req;
+  res.json({ path });
 };
